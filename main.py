7aa23077695b6943ff60fff1005ffdd334c1c4c0
Sara Williams <sara.williams.n@gmail.com>
2021-12-03 14:54:40 +0000

    bot will send a 'test' message every 5 seconds for 5 times

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import os
 import discord
+from discord.ext import tasks
 
 client = discord.Client()
 
-@client.event
-async def on_ready():
-  print('I am online as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
@@ -14,6 +13,20 @@ async def on_message(message):
 
   if message.content.startswith('$hello'):
     await message.channel.send('Guten Tag!')
+  
+  if message.content.startswith('$sit'):
+    slow_count.start(message.channel)
+
+
+@tasks.loop(seconds=5.0, count=5)
+async def slow_count(channel):
+    print(slow_count.current_loop)
+    await channel.send('test')
+
+@slow_count.after_loop
+async def after_slow_count():
+    print('done!')
+    
 
 client.run(os.environ['bot_key'])
 
